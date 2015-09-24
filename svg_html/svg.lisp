89cf672911a5aa0f87755760a2224c974c754fed
Benjamin Hipple <brh2113@columbia.edu>
2015-09-24 00:17:02 -0400

    Started work on the macro-based DSL for svg+html

---
+++ svg_html/svg.lisp
@@ -0,0 +1,50 @@
+; This provides a DSL for writing XML, HTML, and SVG tags
+; built on Lisp macros
+
+;; ============================================================================
+;;                                  Macros
+;; ============================================================================
+(defmacro let1 (var val &body body)
+  `(let ((,var ,val))
+     ,@body))
+
+(defmacro split (val yes no)
+  (let1 g (gensym)
+        `(let1 ,g ,val
+               (if ,g
+                 (let ((head (car ,g))
+                       (tail (cdr ,g)))
+                   ,yes)
+                 ,no))))
+
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                                     (pairs atts)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
+
+;; ============================================================================
+;;                             Helper Functions
+;; ============================================================================
+(defun pairs (lst)
+  (labels ((f (lst acc)
+              (split lst
+                     (if tail
+                       (f (cdr tail) (cons (cons head (car tail)) acc))
+                       (reverse acc))
+                     (reverse acc))))
+    (f lst nil)))
+
+(defun print-tag (name alst closingp)
+  (princ #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (att)
+          (format t " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
+        alst)
+  (princ #\>))
+
