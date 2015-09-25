1b826d68b82293696f3837a652a625305d0bc96d
Benjamin Hipple <brh2113@columbia.edu>
2015-09-24 22:34:56 -0400

    Finished SVG DSL

--- svg_html/svg.lisp
+++ svg_html/svg.lisp
@@ -26,6 +26,19 @@
           ,@body
           (print-tag ',name nil t)))
 
+(defmacro html (&body body)
+  `(tag html ()
+        ,@body))
+
+(defmacro body (&body body)
+  `(tag body ()
+        ,@body))
+
+(defmacro svg (&body body)
+  `(tag svg (xmlns "http://www.w3.org/2000/svg"
+                   "xmlns:xlink" "http://www.w3.org/1999/xlink")
+        ,@body))
+
 ;; ============================================================================
 ;;                             Helper Functions
 ;; ============================================================================
@@ -48,3 +61,48 @@
         alst)
   (princ #\>))
 
+(defun brightness (col amt)
+  (mapcar (lambda (x)
+            (min 255 (max 0 (+ x amt))))
+          col))
+
+(defun svg-style (color)
+ (format nil
+  "~{fill:rgb(~a,~a,~a);stroke:rgb(~a,~a,~a)~}"
+  (append color
+   (brightness color -100))))
+
+(defun circle (center radius color)
+  (tag circle (cx (car center)
+                  cy (cdr center)
+                  r radius
+                  style (svg-style color))))
+
+(defun polygon (points color)
+  (tag polygon (points (format nil
+                               "~{~a,~a ~}"
+                               (mapcan (lambda (tp)
+                                         (list (car tp) (cdr tp)))
+                                       points))
+                       style (svg-style color))))
+
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                         (1- value)
+                         (1+ value))
+                       (1- length)))))
+
+(defun generate-random-walk-graph ()
+  (with-open-file (*standard-output* "random_walk.svg"
+                                     :direction :output
+                                     :if-exists :supersede)
+    (svg (loop repeat 10
+               do (polygon (append '((0 . 200))
+                                   (loop for x
+                                         for y in (random-walk 100 400)
+                                         collect (cons x y))
+                                   '((400 . 200)))
+                           (loop repeat 3
+                                 collect (random 256)))))))
