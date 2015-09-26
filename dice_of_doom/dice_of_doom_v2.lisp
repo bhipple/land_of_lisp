f424a5c77cb9fccb2893b649bc589f2642fd1b13
Benjamin Hipple <brh2113@columbia.edu>
2015-09-26 18:54:08 -0400

    Finished lazy evaluation and alpha-beta purning for DoD

--- dice_of_doom/dice_of_doom_v2.lisp
+++ dice_of_doom/dice_of_doom_v2.lisp
@@ -2,18 +2,20 @@
 (load "dice_of_doom_v1.lisp")
 (load "../lazy_evaluation/lazy.lisp")
 
-(defparameter *board-size* 4)
+(defparameter *board-size* 5)
 (defparameter *board-hexnum* (* *board-size* *board-size*))
+
+(defparameter *ai-level* 4)
 
 (defun add-passing-move (board player spare-dice first-move moves)
   (if first-move
     moves
     (lazy-cons (list nil
-                (game-tree (add-new-dice board player (1- spare-dice))
-                           (mod (1+ player) *num-players*)
-                           0
-                           t))
-          moves)))
+                     (game-tree (add-new-dice board player (1- spare-dice))
+                                (mod (1+ player) *num-players*)
+                                0
+                                t))
+               moves)))
 
 (defun attacking-moves (board cur-player spare-dice)
   (labels ((player (pos)
@@ -25,19 +27,24 @@
         (if (eq (player src) cur-player)
           (lazy-mapcan
             (lambda (dst)
-              (if (and (not (eq (player dst) cur-player))
-                         (> (dice src) (dice dst)))
+              (if (and (not (eq (player dst)
+                                cur-player))
+                       (> (dice src) (dice dst)))
                 (make-lazy
-                  (list (list src dst)
-                        (game-tree (board-attack board cur-player src dst (dice src))
-                                   cur-player
-                                   (+ spare-dice (dice dst))
-                                   nil))))
-              (lazy-nil)))
-          (make-lazy (neighbors src)))
-        (lazy-nil)))
-    (make-lazy (loop for n below *board-hexnum*
-                     collect n))))
+                  (list (list (list src dst)
+                              (game-tree (board-attack board 
+                                                       cur-player 
+                                                       src 
+                                                       dst 
+                                                       (dice src))
+                                         cur-player
+                                         (+ spare-dice (dice dst))
+                                         nil))))
+                (lazy-nil)))
+            (make-lazy (neighbors src)))
+          (lazy-nil)))
+      (make-lazy (loop for n below *board-hexnum*
+                       collect n)))))
 
 (defun handle-human (tree)
   (fresh-line)
@@ -63,3 +70,104 @@
     (play-vs-human (handle-human tree))
     (announce-winner (cadr tree))))
 
+;; ============================================================================
+;;                              AI Improvements
+;; ============================================================================
+(defun limit-tree-depth (tree depth)
+  (list (car tree)
+        (cadr tree)
+        (if (zerop depth)
+          (lazy-nil)
+          (lazy-mapcar (lambda (move)
+                         (list (car move)
+                               (limit-tree-depth (cadr move) (1- depth))))
+                       (caddr tree)))))
+
+; Tiles that are owned by the player and not in danger of being re-taken
+; are worth more than tiles that are threatened
+(defun score-board (board player)
+  (loop for hex across board
+        for pos from 0
+        sum (if (eq (car hex) player)
+              (if (threatened pos board)
+                1
+                2)
+              -1)))
+
+(defun threatened (pos board)
+  (let* ((hex (aref board pos))
+         (player (car hex))
+         (dice (cadr hex)))
+    (loop for n in (neighbors pos)
+          do (let* ((nhex (aref board n))
+                    (nplayer (car nhex))
+                    (ndice (cadr nhex)))
+               (when (and (not (eq player nplayer)) (> ndice dice))
+                 (return t))))))
+
+(defun get-ratings (tree player)
+  (take-all (lazy-mapcar (lambda (move)
+                           (rate-position (cadr move) player))
+                         (caddr tree))))
+
+(defun rate-position (tree player)
+  (let ((moves (caddr tree)))
+    (if (not (lazy-null moves))
+      (apply (if (eq (car tree) player)
+               #'max
+               #'min)
+             (get-ratings tree player))
+      (score-board (cadr tree) player))))
+
+; Alpha-beta pruning
+(defun ab-get-ratings-max (tree player upper-limit lower-limit)
+  (labels ((f (moves lower-limit)
+              (unless (lazy-null moves)
+                (let ((x (ab-rate-position (cadr (lazy-car moves))
+                                           player
+                                           upper-limit
+                                           lower-limit)))
+                  (if (>= x upper-limit)
+                    (list x)
+                    (cons x (f (lazy-cdr moves) (max x lower-limit))))))))
+    (f (caddr tree) lower-limit)))
+
+(defun ab-get-ratings-min (tree player upper-limit lower-limit)
+  (labels ((f (moves upper-limit)
+              (unless (lazy-null moves)
+                (let ((x (ab-rate-position (cadr (lazy-car moves))
+                                           player
+                                           upper-limit
+                                           lower-limit)))
+                  (if (<= x lower-limit)
+                    (list x)
+                    (cons x (f (lazy-cdr moves) (min x upper-limit))))))))
+    (f (caddr tree) upper-limit)))
+
+(defun ab-rate-position (tree player upper-limit lower-limit)
+  (let ((moves (caddr tree)))
+    (if (not (lazy-null moves))
+      (if (eq (car tree) player)
+        (apply #'max (ab-get-ratings-max tree
+                                         player
+                                         upper-limit
+                                         lower-limit))
+        (apply #'min (ab-get-ratings-min tree
+                                         player
+                                         upper-limit
+                                         lower-limit)))
+      (score-board (cadr tree) player))))
+
+(defun handle-computer (tree)
+  (let ((ratings (ab-get-ratings-max (limit-tree-depth tree *ai-level*)
+                                     (car tree)
+                                     most-positive-fixnum
+                                     most-negative-fixnum)))
+    (cadr (lazy-nth (position (apply #'max ratings) ratings)
+                    (caddr tree)))))
+
+(defun play-vs-computer (tree)
+  (print-info tree)
+  (cond ((lazy-null (caddr tree)) (announce-winner (cadr tree)))
+        ((zerop (car tree)) (play-vs-computer (handle-human tree)))
+        (t (play-vs-computer (handle-computer tree)))))
